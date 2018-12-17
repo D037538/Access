745194b27cb69e33f962b918de922a799c126e92
anusonawane94@gmail.com <anusonawane94@gmail.com>
2018-12-17 20:55:48 +0530

    my first jwt

---
+++ src/main/java/auth/Constants.java
@@ -0,0 +1,16 @@
+package auth;
+
+
+
+public class Constants {
+
+    public static String ANU_API_KEY = "YnjYUKBcfOCGG2C";
+
+    public static final String AUTH_TOKEN = "AUTH-TOKEN";
+    public static final String API_KEY = "API-KEY";
+
+    public static String SESSION_USER = "user";
+    public static String SESSION_SUB_USER = "sub_user";
+
+
+}
